db78552750bf9b6e84677c9abf82522f57ac922a
Felipe Becker <62967128+flpBkr@users.noreply.github.com>
2021-01-21 14:11:53 -0300

    Starting cep api setting

--- flp-app-login/frontend/src/pages/ListForm.js
+++ flp-app-login/frontend/src/pages/ListForm.js
@@ -1,45 +1,78 @@
 import React from 'react';
 
 import './Pages.css';
-
+// {
+//   "cep": "12235-440",
+//   "logradouro": "Rua Cosmos",
+//   "complemento": "",
+//   "bairro": "Jardim Paraíso",
+//   "localidade": "São José dos Campos",
+//   "uf": "SP",
+//   "ibge": "3549904",
+//   "gia": "6452",
+//   "ddd": "12",
+//   "siafi": "7099"
+// }
 export default function ListForm(){
+    function onBlurCep(e) {
+      const { value } = e.target;
+
+      const cep = value.replace(/[^0-9]/g, "");
+
+      if (value?.length !== 8) {
+        return;
+      }
+      fetch(`https://viacep.com.br/ws/${value}/json/`)
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+    }
     return (
       <div className="list-form">
         <h4>Cadastrar Clientes</h4>
 
         <label className="form-label">Nome</label>
         <div className="input-group input-group-sm mb-3">
-          <input type="text" className="form-control" placeholder="Nome" aria-label="Nome" aria-describedby="basic-addon1" />
+          <input name="name" type="text" className="form-control" placeholder="Nome" />
         </div>
 
-        <label className="form-label">Endereço</label>
+        <label className="form-label">Cep</label>
         <div className="input-group input-group-sm mb-3">
-          <input type="text" className="form-control" placeholder="Nome da Rua, Número" aria-label="Nome da Rua, Número" />
+          <input name="cep" type="text" className="form-control" placeholder="CEP" onBlur={onBlurCep}/>
+        </div>
+
+        <label className="form-label">Rua</label>
+        <div className="input-group input-group-sm mb-3">
+          <input name="logradouro" type="text" className="form-control" placeholder="Rua"/>
         </div>
 
         <label className="form-label">Bairro</label>
         <div className="input-group input-group-sm mb-3">
-          <input type="text" className="form-control" placeholder="Bairro" aria-label="Bairro" />
+          <input name="bairro" type="text" className="form-control" placeholder="Bairro" aria-label="Bairro" />
         </div>
 
         <label className="form-label">Cidade</label>
         <div className="input-group input-group-sm mb-3">
-          <input type="text" className="form-control" placeholder="Cidade" aria-label="Cidade" />
+          <input name="localidade" type="text" className="form-control" placeholder="Cidade" />
         </div>
 
         <label className="form-label">UF</label>
         <div className="input-group input-group-sm mb-3">
-          <input type="text" className="form-control" placeholder="UF" aria-label="UF" />
+          <input name="uf" type="text" className="form-control" placeholder="UF" />
+        </div>
+
+        <label className="form-label">DDD</label>
+        <div className="input-group input-group-sm mb-3">
+          <input name="ddd" type="tel" className="form-control" placeholder="Código de área" />
         </div>
 
         <label className="form-label">Telefone</label>
         <div className="input-group input-group-sm mb-3">
-          <input type="tel" className="form-control" placeholder="Código de área e número do telefone" aria-label="Código de área e número do telefone" />
+          <input type="tel" className="form-control" placeholder="Número do telefone" />
         </div>
 
         <label className="form-label">Email</label>
         <div className="input-group input-group-sm mb-3">
-          <input type="email" className="form-control" placeholder="Insira um email válido" aria-label="Insira um email válido" aria-describedby="basic-addon2" />
+          <input name="email" type="email" className="form-control" placeholder="Insira um email válido" />
         </div>
 
         <div className="input-group mb-3">
@@ -50,10 +83,3 @@ export default function ListForm(){
       </div>
     );
 }
-        {/* <div className="header">
-          <form>
-            <input placeholder="enter task">
-            </input>
-            <button class="btn btn-primary" type="submit">add</button>
-          </form>
-        </div> */}
